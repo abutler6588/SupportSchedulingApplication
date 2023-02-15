a14921e8213582d9c76b9ef004c89156e759377d
Andre Butler <andre.butler@ad.infosys.com>
2023-02-15 17:08:11 -0600

    first commit

---
+++ src/main/java/com/infosys/Support_Scheduling/SupportSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.Support_Scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/Support_Scheduling/SupportSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.Support_Scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
